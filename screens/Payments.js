7feb27c5c1b552d0eca41504e5499d32c17b596d
karimsh2001 <karimsh2001@gmail.com>
2023-11-09 14:35:54 -0400

    front de los pagos mostrados

--- screens/Payments.js
+++ screens/Payments.js
@@ -1,52 +1,53 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, FlatList, TouchableOpacity, Date } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import moment from 'moment';
 
-
-const Payments = ({route}) => {
+const Payments = ({ route, navigation }) => {
   const data = route.params.data;
   let selectedDate = moment(data);
   selectedDate = selectedDate.format('DD/MM/YYYY');
-  const navigation = useNavigation();
 
-
-  const samplePayments = [
-    { id: 1, amount: 100, description: 'Payment for item A' },
-    { id: 2, amount: 50, description: 'Payment for item B' },
-    { id: 3, amount: 200, description: 'Payment for item C' },
+  // Sample payment data for demonstration
+  const payments = [
+    { ref: 'Ref 1', phoneNumber: '123-456-7890', amount: 50 },
+    { ref: 'Ref 2', phoneNumber: '987-654-3210', amount: 75 },
+    { ref: 'Ref 3', phoneNumber: '111-222-3333', amount: 100 },
+    // ... other payment data
   ];
 
-  const payments = samplePayments;
-  const handleBackPress = () => {
-    navigation.goBack();
-  };
-
-  const renderPaymentItem = ({ item }) => {
+  const renderRow = ({ item, index }) => {
+    const rowStyle = index % 2 === 0 ? styles.rowEven : styles.rowOdd;
     return (
-      <View style={styles.paymentItem}>
-        <Text style={styles.paymentAmount}>{item.amount}</Text>
-        <Text style={styles.paymentDescription}>{item.description}</Text>
+      <View style={[styles.row, rowStyle]}>
+        <Text style={styles.rowData}>{item.ref}</Text>
+        <Text style={styles.rowData}>{item.phoneNumber}</Text>
+        <Text style={styles.rowData}>{item.amount}</Text>
       </View>
     );
   };
 
+  const goBack = () => {
+    navigation.goBack();
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.headerContainer}>
-        <TouchableOpacity style={styles.backButton} onPress={handleBackPress}>
-          <Ionicons name="arrow-back" size={24} color="black" />
-        </TouchableOpacity>
-        <Text style={styles.headerTitle}>Pagos del {selectedDate}</Text>
+      <Text style={styles.title}>Pagos del día {selectedDate}</Text>
+      <View style={styles.table}>
+        <View style={styles.tableHeader}>
+          <Text style={styles.tableTitle}>Referencia</Text>
+          <Text style={styles.tableTitle}>Nro. Celular</Text>
+          <Text style={styles.tableTitle}>Monto (Bs)</Text>
+        </View>
+        <FlatList
+          data={payments}
+          keyExtractor={(_, index) => index.toString()}
+          renderItem={renderRow}
+        />
       </View>
-
-      <FlatList
-        data={payments}
-        renderItem={renderPaymentItem}
-        keyExtractor={item => item.id}
-        style={styles.paymentsList}
-      />
+      <TouchableOpacity style={styles.backButton} onPress={goBack}>
+        <Text style={styles.backButtonText}>Regresar</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -54,47 +55,75 @@ const Payments = ({route}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#363062',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    paddingTop: 50,
+    backgroundColor: '#FFF',
+    padding: 20,
   },
-  headerContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 20,
-  },
-  backButton: {
-    marginRight: 20,
-  },
-  headerTitle: {
+  title: {
     color: '#F99417',
-    fontSize: 24,
+    fontSize: 36,
     fontWeight: '700',
     textAlign: 'center',
     textShadowColor: 'rgba(0, 0, 0, 0.5)',
     textShadowOffset: { width: 1, height: 1 },
     textShadowRadius: 3,
+    paddingTop: 0,
+    marginBottom: 20,
   },
-  paymentsList: {
-    width: '100%',
-    flex: 1,
+  table: {
+    backgroundColor: '#F0F0F0',
+    padding: 10,
+    borderRadius: 10,
+    marginTop: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
-  paymentItem: {
+  tableHeader: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center',
-    backgroundColor: 'white',
-    borderRadius: 10,
-    padding: 10,
-    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#DDD',
+    paddingVertical: 10,
+    backgroundColor: '#8280A3',
   },
-  paymentAmount: {
+  tableTitle: {
     fontWeight: 'bold',
-    fontSize: 18,
+    flex: 1,
+    textAlign: 'center',
+    color: 'white',
   },
-  paymentDescription: {
-    fontSize: 16,
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomWidth: 1,
+    borderBottomColor: '#DDD',
+    paddingVertical: 10,
+  },
+  rowEven: {
+    backgroundColor: '#C4C3CF',
+  },
+  rowOdd: {
+    backgroundColor: 'white',
+  },
+  rowData: {
+    flex: 1,
+    textAlign: 'center',
+  },
+  backButton: {
+    backgroundColor: '#363062',
+    padding: 15,
+    borderRadius: 10,
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  backButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
 
